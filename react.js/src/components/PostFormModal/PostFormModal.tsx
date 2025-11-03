0678e5b68e6a4ab0928d63b524e16cceaf1bac9d
Yohann <yoyo24du26@gmail.com>
2025-11-03 15:33:31 +0100

    feat(PostFormModal): using `Input` & `Textarea` component

--- react.js/src/components/PostFormModal/PostFormModal.tsx
+++ react.js/src/components/PostFormModal/PostFormModal.tsx
@@ -5,6 +5,8 @@ import type { PostFormModalInterface } from "./PostFormModal.interface";
 import Button from "../Button/Button";
 
 import style from "./PostFormModal.module.scss";
+import Input from "../Input/Input";
+import Textarea from "../Textarea/Textarea";
 
 export default function PostFormModal({ post, onClose }: PostFormModalInterface) {
     const [input, setInput] = useState(post?.title ?? "");
@@ -29,22 +31,21 @@ export default function PostFormModal({ post, onClose }: PostFormModalInterface)
             <span className={style.blurredBg}></span>
 
             <div className={style.modal}>
-                <div>
-                    <label htmlFor="title">Title</label>
-                    <input
-                        id="title"
-                        value={input}
-                        onChange={(e) => setInput(e.target.value)}
-                    />
-                </div>
-                <div>
-                    <label htmlFor="content">Content</label>
-                    <textarea
-                        id="content"
-                        value={textarea}
-                        onChange={(e) => setTextarea(e.target.value)}
-                    ></textarea>
-                </div>
+                <Input
+                    id="title"
+                    label="Title"
+                    type="text"
+                    placeholder="Title name"
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)}
+                />
+                <Textarea
+                    id="content"
+                    label="Content"
+                    placeholder="Content"
+                    value={textarea}
+                    onChange={(e) => setTextarea(e.target.value)}
+                />
                 <div className={style.modalCta}>
                     <Button type="button" variant="buttonBlack" action={close} size="m">Cancel</Button>
                     <Button type="button" variant="button" action={save} size="m">Save</Button>
