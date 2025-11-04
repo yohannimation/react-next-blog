5e5af21aac6ab716f33fc95f5cb818026af7e6c3
Yohann <yoyo24du26@gmail.com>
2025-11-04 11:43:22 +0100

    feat(Header): using `UserContext` to display button

--- react.js/src/components/Header/Header.tsx
+++ react.js/src/components/Header/Header.tsx
@@ -1,17 +1,31 @@
 import { Link } from "react-router-dom";
 
+import { useUser } from "../../context/UserContext";
+
 import style from "./Header.module.scss"
 
 import Button from "../Button/Button";
 
 export default function Header() {
+    const { user, logout } = useUser();
+
     return (
         <header className={style.header}>
             <Link to="/">Logo</Link>
 
             <div className={style.cta}>
-                <Button type="link" variant="link" href="/login">Login</Button>
-                <Button type="link" variant="button" href="/register" size="s">Register</Button>
+                {
+                    user ?
+                    <>
+                        <p>compte</p>
+                        <Button type="button" variant="buttonBlack" action={logout} size="s">Login</Button>
+                    </>
+                    :
+                    <>
+                        <Button type="link" variant="link" href="/login">Login</Button>
+                        <Button type="link" variant="button" href="/register" size="s">Register</Button>
+                    </>
+                }
             </div>
         </header>
     )
